f60f0466ce9d41514406d2030a662e2944ba0486
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-30 22:15:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
 
   def self.create_from_filename(mp3_formatted_file)
     c = self.new
-    c.name = mp3_formatted_file.split(/[^a-zA-Z\s] |\s-\s/)[1]
+    c.name = mp3_formatted_file.split(/[^a-zA-Z\s] |\s-\s/)[0]
     c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s] |\s-\s/)[0]
     c.save
     c
